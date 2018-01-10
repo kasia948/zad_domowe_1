45e5d9cf0082e45d9752b2f6037e24033289c39e
Kasia RN <rudyknowak@gmail.com>
2018-01-10 17:50:37 +0100

    trzecia wersja

--- src/Zad_java_1.java
+++ src/Zad_java_1.java
@@ -5,5 +5,6 @@ public class Zad_java_1 {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("wersja trzy");
     }
 }
